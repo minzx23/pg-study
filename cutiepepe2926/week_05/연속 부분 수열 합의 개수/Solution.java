b1cabca6a5743b8046e6aaf2747e58201049e3dc
cutiepepe29266 <cutiepepe2926@gmail.com>
2026-05-03 21:04:40 +0900

    [PGS] 연속 부분 수열 합의 개수 / 이건희

---
+++ cutiepepe2926/week_05/연속 부분 수열 합의 개수/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+
+        int[] answer = {0, sequence.length - 1};
+
+        int left = 0;
+        int sum = 0;
+
+        for (int right = 0; right < sequence.length; right++) {
+
+            sum += sequence[right];
+
+            while (sum > k) {
+                sum -= sequence[left++];
+            }
+
+            if (sum == k) {
+                if (right - left < answer[1] - answer[0]) {
+                    answer[0] = left;
+                    answer[1] = right;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
