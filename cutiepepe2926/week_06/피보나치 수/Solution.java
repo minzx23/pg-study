64545246796b4a502dc341b9e9999b9240ef0168
cutiepepe29266 <cutiepepe2926@gmail.com>
2026-05-18 20:39:14 +0900

    [PGS] 피보나치 수 / 이건희

---
+++ cutiepepe2926/week_06/피보나치 수/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+
+        int[] memo = new int[100001];
+
+        memo[0] = 0;
+        memo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            memo[i] = (memo[i-1] + memo[i-2]) % 1234567;
+
+        }
+
+        return memo[n];
+
+    }
+
+}
